10b92038c80c9923be6b0e9efca15da80f5ad125
antoinerzad <antoinerzad@gmail.com>
2023-02-28 14:10:50 +0100

    Update Telecablesat.php

--- src/Component/Provider/Telecablesat.php
+++ src/Component/Provider/Telecablesat.php
@@ -14,7 +14,7 @@ use racacax\XmlTv\ValueObject\Program;
 class Telecablesat extends AbstractProvider implements ProviderInterface
 {
     private static $cache = []; // multiple channels are on the same page
-    private static $BASE_URL = 'https://tv-programme.telecablesat.fr';
+    private static $BASE_URL = 'https://tvcablesatproxy.antoiner.workers.dev';
     private $loopCounter = 0;
     public function __construct(Client $client, ?float $priority = null)
     {
